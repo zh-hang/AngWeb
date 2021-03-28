1e389822d78eb2fecbb9453de2b1adafcb4a7341
zh-hang <zhangh127@163.com>
2021-03-28 20:52:43 +0800

    增加了socket库

--- scr/ang.cpp
+++ scr/ang.cpp
@@ -1,3 +1,13 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+
 int main(){
+    int listenfd,connfd;
+    socket_t clientlen;
+    struct sockaddr_storage clientaddr;
+    char client_hostname[MAXHOSTNAME],client_port[MAXHOSTNAME];
+
+    
+
     return 0;
 }
